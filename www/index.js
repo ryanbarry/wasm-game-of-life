4fc178552e5ccc38dd42fc95f43f7fdc3de3d499
Ryan Barry <ryan@nuclearice.com>
2020-03-09 21:38:38 -0700

    factor out toggling cell at client coords

--- www/index.js
+++ www/index.js
@@ -19,20 +19,23 @@ const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
-canvas.addEventListener("click", event => {
+const toggleCellAtClientXY = (clientX, clientY) => {
     const boundingRect = canvas.getBoundingClientRect();
 
     const scaleX = canvas.width / boundingRect.width;
     const scaleY = canvas.height / boundingRect.height;
 
-    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
-    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+    const canvasLeft = (clientX - boundingRect.left) * scaleX;
+    const canvasTop = (clientY - boundingRect.top) * scaleY;
 
     const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
     const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
 
     universe.toggle_cell(row, col);
+};
 
+canvas.addEventListener("click", event => {
+    toggleCellAtClientXY(event.clientX, event.clientY);
     drawCells();
 });
 
